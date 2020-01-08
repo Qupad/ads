24d8b706cd82b5ebde844d96eeeedc9201e785b2
Qupad <capzed@gmail.com>
2020-01-08 18:18:26 +0300

    Created dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,11 +1,25 @@
 ActiveAdmin.register_page "Dashboard" do
-  menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
+  menu priority: 1
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Recently published advertise" do
+          table_for Article.where(life_cycle: 'published').order("id desc").limit(10) do |article|
+            column("Title") { |article| article.title }
+            column("Text") { |article| article.text }
+            column("Images") do |article| 
+              ul do
+                article.images.each do |i|
+                  li do
+                    image_tag(i, style: 'width:100%') 
+                  end
+                end
+              end
+            end
+            column :user
+          end
+        end
       end
     end
 
