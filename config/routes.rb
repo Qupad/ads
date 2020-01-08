9a4a5c4e3dedd4a0ded47094f369650ad0943a47
Qupad <capzed@gmail.com>
2020-01-08 18:23:03 +0300

    New routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :articles
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  resources :categories
+  resources :articles
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
     registrations: 'users/registrations'
 	}
+
+  delete 'articles/:id/delete_image_attachment' => 'articles#delete_image_attachment', as: 'delete_image_attachment'
+
   get 'persons/profile'
 
   get 'articles/:id/send_to_approve' => 'articles#send_to_approve', as: 'send_to_approve'
 
-  
   root 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
