bf439fc8f21d652c902765a35a9422c362ff8d52
Qupad <capzed@gmail.com>
2020-01-28 17:41:59 +0300

    add localization (en, ru)

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  resources :categories
-  resources :articles
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    passwords: 'users/passwords',
-    registrations: 'users/registrations'
-	}
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do 
+    devise_for :admin_users, ActiveAdmin::Devise.config
+    ActiveAdmin.routes(self)
+    resources :categories
+    devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      passwords: 'users/passwords',
+      registrations: 'users/registrations'
+  	}
 
-  delete 'articles/:id/delete_image_attachment' => 'articles#delete_image_attachment', as: 'delete_image_attachment'
+    resources :articles
 
-  get 'persons/profile'
+    delete 'articles/:id/delete_image_attachment' => 'articles#delete_image_attachment', as: 'delete_image_attachment'
 
-  get 'articles/:id/send_to_approve' => 'articles#send_to_approve', as: 'send_to_approve'
+    get 'persons/profile'
 
-  root 'articles#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get 'articles/:id/send_to_approve' => 'articles#send_to_approve', as: 'send_to_approve'
+
+    root 'articles#index'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
